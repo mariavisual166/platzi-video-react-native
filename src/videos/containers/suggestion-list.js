e3f20290fcd562f5378a17f53ab4bdd9d64dd540
Mariangela <mariavisual166@gmail.com>
2020-01-16 22:25:48 -0400

    first commit

---
+++ src/videos/containers/suggestion-list.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  FlatList,
+  Text
+} from 'react-native';
+import Layout from '../components/suggestion-list-layout';
+import Empty from '../components/empty';
+import Separator from '../components/vertical-separator';
+import Suggestion from '../components/suggestion';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+  return {
+    list: state.suggestionList
+  }
+}
+
+class SuggestionList extends Component {
+  keyExtractor = item => item.id.toString()
+  renderEmtpy = () => <Empty text="No hay sugerencias :(" />
+  itemSeparator = () => <Separator />
+  renderItem = ({item}) => {
+    return (
+      <Suggestion {...item}/>
+    )
+  }
+  render() {
+
+    return (
+      <Layout
+        title="Recomendado para ti"
+        >
+        <FlatList
+          keyExtractor={this.keyExtractor}
+          data={this.props.list}
+          ListEmptyComponent={this.renderEmtpy}
+          ItemSeparatorComponent={this.itemSeparator}
+          renderItem={this.renderItem}
+        />
+      </Layout>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(SuggestionList)
